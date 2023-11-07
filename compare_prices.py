ceefe967513c33d260fc0fe99ae7b2547cde5df3
vihanga02 <muthumalavdw.22@uom.lk>
2023-11-07 23:31:28 +0530

    learning stage

--- compare_prices.py
+++ compare_prices.py
@@ -11,8 +11,10 @@ user_agent = {'User-agent': 'Mozilla/5.0'}
 
 def compare_prices(product_laughs,product_glomark):
     #TODO: Aquire the web pages which contain product Price
-    re
+    laughs_response = requests.get(product_laughs, headers=user_agent)
+    glomark_response = requests.get(product_glomark, headers=user_agent)
 
+    
     
     
     #TODO: LaughsSuper supermarket website provides the price in a span text.
@@ -24,12 +26,12 @@ def compare_prices(product_laughs,product_glomark):
     
     #TODO: Parse the values as floats, and print them.
     
-    print('Laughs  ',product_name_laughs,'Rs.: ' , price_laughs)
-    print('Glomark ',product_name_glomark,'Rs.: ' , price_glomark)
+    # print('Laughs  ',product_name_laughs,'Rs.: ' , price_laughs)
+    # print('Glomark ',product_name_glomark,'Rs.: ' , price_glomark)
     
-    if(price_laughs>price_glomark):
-        print('Glomark is cheaper Rs.:',price_laughs - price_glomark)
-    elif(price_laughs<price_glomark):
-        print('Laughs is cheaper Rs.:',price_glomark - price_laughs)    
-    else:
-        print('Price is the same')
+    # if(price_laughs>price_glomark):
+    #     print('Glomark is cheaper Rs.:',price_laughs - price_glomark)
+    # elif(price_laughs<price_glomark):
+    #     print('Laughs is cheaper Rs.:',price_glomark - price_laughs)    
+    # else:
+    #     print('Price is the same')
---
+++ learn.py
@@ -0,0 +1,22 @@
+#importing libraries
+from bs4 import BeautifulSoup
+import requests
+
+#initiating the websit's url
+websit = "https://subslikescript.com/movie/Titanic-120338"
+
+#geting contents from the website
+result = requests.get(websit)
+content = result.text
+
+#geting htm contents of the website
+soup = BeautifulSoup(content, 'lxml')
+
+#get the contents of the article on the webpage
+box = soup.find('article' , class_ = 'main-article')
+
+#get the heading of the article
+head = soup.find('h1').get_text()
+
+#get the transcropt as a text
+htranscript= soup.find('div',class_='full-script').get_text(strip = True, separator=' ')
